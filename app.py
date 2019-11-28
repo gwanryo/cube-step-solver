0717edcb1b5797204f468cfbb0e3d63cea8affb8
Ryo <9062624+gwanryo@users.noreply.github.com>
2019-11-28 17:57:37 +0900

    Add proxy

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from root import root
 from cube import cube
+from video import video
 
 app = Flask(__name__, static_folder='templates/cube-simulator/static')
 app.register_blueprint(root)
 app.register_blueprint(cube, url_prefix='/cube')
+app.register_blueprint(video, url_prefix='/video')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
---
+++ video.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, request, url_for, abort
+from requests import get
+
+video = Blueprint('video', __name__)
+
+VIDEO_URL = [
+    'http://localhost:8080/?action=stream',
+    'http://localhost:8081/?action=stream'
+]
+
+@video.route('/<id>')
+def videos(order):
+    if order < len(VIDEO_URL):
+        return get(f'{VIDEO_URL[order]}').content
+    else
+        abort(404)
